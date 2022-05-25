4cdd14abc030d262046b569d7275451898202165
Obianuju Onwuasoanya <onobes007@gmail.com>
2022-05-25 23:00:23 +0100

    Update reading_file.py

--- reading_file.py
+++ reading_file.py
@@ -1,15 +1,5 @@
-# Read text from a file, and count the occurence of words in that text
-# Example:
-# count_words("The cake is done. It is a big cake!") 
-# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
-
 # from curses import newwin
-
-
-# from curses import newwin
-
-
-# from curses import newwin
+import string
 
 
 def read_file_content(filename):
@@ -23,12 +13,13 @@ d = dict()
 def count_words(text):
     text = read_file_content(text)
     # [assignment] Add your code here
-    line = text.strip().lower().split(" ")
+    char_punct = text.translate(str.maketrans('','', string.punctuation))
+    line = char_punct.strip().lower().split(" ")
     for newWord in line: 
         if newWord in d:
             d[newWord] = d[newWord] + 1
         else:
-                d[newWord] = 1   
+            d[newWord] = 1   
     return d
 
 print(count_words('story'))
